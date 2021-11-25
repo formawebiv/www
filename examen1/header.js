ec15b216d8a7dffc58805f6292402ef7ad61d978
= <=>
2021-11-25 13:07:34 +0100

    okey

---
+++ examen1/header.js
@@ -0,0 +1,56 @@
+const headerTemplate = document.createElement('template');
+
+headerTemplate.innerHTML = `
+        <style>
+          nav {
+            height: 70px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-color:  #0a0a23;
+          }
+  
+          ul {
+            display: flex;
+            padding: 0;
+          }
+
+          li {
+            padding-left: 100px;
+            list-style: none;
+          }
+          
+          a {
+            font-weight: 700;
+            margin: 0 0px;
+            color: #fff;
+            text-decoration: none;
+          }
+          
+          a:hover {
+            padding-bottom: 0px;
+            box-shadow: inset 0 -2px 0 0 #fff;
+          }
+        </style>
+        <header>
+          <nav>
+            <ul>
+              <li><a href="../examen1/about.html">Contenido</a></li>
+              <li><a href="../examen1/index.html">Inicio</a></li>
+              <li><a href="contact.html">Contact</a></li>
+            </ul>
+          </nav>
+        </header>
+      `;      
+      class Header extends HTMLElement {
+        constructor() {
+          super();
+        }
+      
+        connectedCallback() {
+          const shadowRoot = this.attachShadow({ mode: 'closed' });
+      
+          shadowRoot.appendChild(headerTemplate.content);
+        }
+    }
+  customElements.define('header-component', Header);
---
+++ examen1/readme.md
@@ -0,0 +1,11 @@
+Menu (header) y footer (con RRSS) fijos y reutilizables con JS.
+
+Creamos un doc.html extra para tener dos paginas entre las que podamos navegar.
+
+Los dos primeros enlaces del menu (Contenido e Incio) esta linkeados y se puede navegar de una pagina a otra por medio de Js (ya que tanto el header como el footer son creados ahí).
+
+Mediante Js creamos un header y un footer que va a ser el mismo en todas las secciones que vayamos añadiendo a la web. Al estar ambos "{ mode: 'closed' }", cuando llames a los elementos que hay dentro desde otro Js no se van a modificar. Para que eso ocurriera tendria que ser "{ mode: 'open' }".
+
+Añadimos los elementos del main (titulo y div (con galeria de imagenes) 
+
+Preparamos los elementos de la pagina con CSS para que sea responsiva y accesible según lo necesitemos.
